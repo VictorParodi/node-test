2c2372d271deb9cf269efe06422ad8aefc8f89ca
Victor <victor.parodi.montero@gmail.com>
2018-01-01 19:07:52 -0500

    master commit 3
    Instalando supertest para testear aplicaciones express. Realizando testing a los endpoints.

--- server/server.js
+++ server/server.js
@@ -4,9 +4,35 @@ let app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send({
+        error: 'Page not found',
+        user: 'Admin'
+    });
+});
+
+app.get('/users', (req, res) => {
+    res.send([
+        {
+            name: 'Allison',
+            age: 25
+        },
+
+        {
+            name: 'Kate',
+            age: 22
+        },
+
+        {
+            name: 'Ana',
+            age: 29
+        }
+    ]);
 });
 
 app.listen(port, () => {
     console.log(`The application is running on port ${port}`);
 });
+
+module.exports = {
+    app
+}
