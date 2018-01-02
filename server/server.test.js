93dcabf75a2d17626de878cfbb849b7658ddbc79
Victor <victor.parodi.montero@gmail.com>
2018-01-01 20:00:41 -0500

    master commit 4
    Usando el método describe() para oraganozar los test cases.

--- server/server.test.js
+++ server/server.test.js
@@ -2,29 +2,35 @@ const request = require('supertest');
 const expect = require('expect');
 const app = require('./server').app;
 
-it('Should return Hello World! response', (done) => {
-    request(app)
-        .get('/')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error: 'Page not found'
-            });
-        })
-        .end(done);
-});
+describe('Server', () => {
+    describe('#root endpoint', () => {
+        it('Should return Hello World! response', (done) => {
+            request(app)
+                .get('/')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found'
+                    });
+                })
+                .end(done);
+        });
+    });
 
-it('Should return an user with name Kate', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                name: 'Kate',
-                age: 22
-            });
-        })
-        .end(done);
+    describe('#users endpoint', () => {
+        it('Should return an user with name Kate', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: 'Kate',
+                        age: 22
+                    });
+                })
+                .end(done);
+        });
+    });
 });
 
 
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,26 +1,36 @@
 const utils = require('./utils');
 const expect = require('expect');
 
-it('Should add two numbers', () => {
-    let res = utils.add(5, 3);
-    expect(res).toBe(8).toBeA('number');
-});
-
-it('Should add two numbers asynchronously', (done) => {
-    utils.addAsync(5, 9, (sum) => {
-        expect(sum).toBe(14).toBeA('number');
-        done();
+describe('Utils', () => {
+    describe('#Add', () => {
+        it('Should add two numbers', () => {
+            let res = utils.add(5, 3);
+            expect(res).toBe(8).toBeA('number');
+        });
     });
-});
 
-it('Should square a number', () => {
-    let res = utils.square(5);
-    expect(res).toBe(25).toBeA('number');
-});
+    describe('#Add asynchronously', () => {
+        it('Should add two numbers asynchronously', (done) => {
+            utils.addAsync(5, 9, (sum) => {
+                expect(sum).toBe(14).toBeA('number');
+                done();
+            });
+        });
+    });
+    
+    describe('#square', () => {
+        it('Should square a number', () => {
+            let res = utils.square(5);
+            expect(res).toBe(25).toBeA('number');
+        });
+    });
 
-it('Should square a number asynchronously', (done) => {
-    utils.sqrtAsync(5, (numSq) => {
-        expect(numSq).toBe(25).toBeA('number');
-        done();
+    describe('#Add asynchronously', () => {
+        it('Should square a number asynchronously', (done) => {
+            utils.sqrtAsync(5, (numSq) => {
+                expect(numSq).toBe(25).toBeA('number');
+                done();
+            });
+        });
     });
 });
